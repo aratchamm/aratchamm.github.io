e0e07f4d7ed98f74c508087a3110252c63fff49b
mm <aratcha.mm@gmail.com>
2020-11-17 00:18:41 +0700

    Edit logo + smooth

--- js/style.js
+++ js/style.js
@@ -1,11 +1,6 @@
 (function($) {
   "use strict";
 
-  $('body').scrollspy({
-      target: '.navbar-fixed-top',
-      offset: 200
-  });
-
   $('#topNav').affix({
       offset: {
           top: 200
@@ -23,5 +18,27 @@
     );                  
 });
  
+//Click Logo To Scroll To Top
+$('#logo').on('click', () => {
+    $('html,body').animate({
+        scrollTop: 0
+    },500);
+});
+
+	//Smooth Scrolling Using Navigation Menu
+	$('a[href*="#"]').on('click', function(e){
+		$('html,body').animate({
+			scrollTop: $($(this).attr('href')).offset().top + 50
+		},500);
+		e.preventDefault();
+    });
+    
+    	//On Scroll Functionality
+	$(window).scroll( () => {
+		var windowTop = $(window).scrollTop();
+		windowTop > 100 ? $('nav').addClass('navShadow') : $('nav').removeClass('navShadow');
+		windowTop > 100 ? $('ul').css('top','100px') : $('ul').css('top','160px');
+	});
+
 
 })(jQuery);
