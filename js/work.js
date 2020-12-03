061b2582634b6f278a80ffa6bac07074f33842fe
mm <aratcha.mm@gmail.com>
2020-12-03 23:04:56 +0700

    Add work page

--- js/work.js
+++ js/work.js
@@ -1,35 +1,5 @@
-let letters = document.getElementsByClassName('title-letter');
+/* This pen use Lettering.js to wrap each letter, nothing more. */
 
-setTimeout(() => {
-  for (let i = 0; i < letters.length; i++) {
-    anime({
-      targets: letters[i],
-      easing: 'easeInQuad',
-      translateX: ['5px', '0'],
-      delay: 50 * i
-    });
-
-    anime({
-      targets: letters[i],
-      easing: 'easeInQuad',
-      opacity: 1,
-      delay: 60 * i,
-      complete: function(anim) {
-        if (i === letters.length - 1) {
-          showSubTitle()
-        }
-      }
-    });
-  }
-}, 500);
-
-
-function showSubTitle() {
-  anime({
-    targets: '#sub-title',
-    easing: 'easeInQuad',
-    opacity: 1,
-    duration: 300,
-    delay: 1
-  });
-}
+$(document).ready(function() {
+	$(".flag").lettering();
+});
