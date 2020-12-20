d616e2a4324ef2d158c4276bc044d9ed64dc5ba5
mm <aratcha.mm@gmail.com>
2020-12-20 18:31:55 +0700

    edit file

--- js/loading.js
+++ js/loading.js
@@ -1,17 +1,32 @@
 
-$(document).ready(function() {
+   if(screen.width < 767 ||
+	navigator.userAgent.match(/Android/i) ||
+	navigator.userAgent.match(/webOS/i) ||
+	navigator.userAgent.match(/iPhone/i) ||
+	navigator.userAgent.match(/iPod/i)) {
+		$(document).ready(function() {
 
-	
-	// Will wait for everything on the page to load.
-	$(window).bind('load', function() {
-		$('.overlay, body').addClass('loaded');
+			// Will wait for everything on the page to load.
+			$(window).bind('load', function() {
+				$('.overlay, body').addClass('loaded');
+				setTimeout(function() {
+					$('.overlay').css({'display':'none'})
+				}, 4000)
+			});
+		})
+	}
+	else $(document).ready(function() {
+		// Will remove overlay for users cannnot load properly.
 		setTimeout(function() {
-			$('.overlay').css({'display':'none'})
-		}, 2000)
-	});
-	
-	// Will remove overlay for users cannnot load properly.
-	setTimeout(function() {
-		$('.overlay, body').addClass('loaded');
-	}, 2000);
-})
+			$('.overlay, body').addClass('loaded');
+		});
+
+		// Will wait for everything on the page to load.
+		$(window).bind('load', function() {
+			$('.overlay, body').addClass('loaded');
+			setTimeout(function() {
+				$('.overlay').css({'display':'none'})
+			}, 4000)
+		});
+
+	})
